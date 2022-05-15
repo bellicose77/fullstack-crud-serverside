76b9819e18e5e0bfe9e08cd891a3cfadcea71e27
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-15 22:55:30 +0600

    server connection

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 5000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://dbuser1:VIDtPXVuRd4GoRGU@cluster0.vtwog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
@@ -10,3 +11,8 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+app.get('/',(req,res)=>{
+    console.log("api connected")
+    res.send("hello world")
+})

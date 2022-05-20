b4755a0fc333e203bee9a802b65a8b6050ddc933
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-20 10:43:08 +0600

    database connect

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const port = 5000;
 
 const uri = "mongodb+srv://dbuser1:VIDtPXVuRd4GoRGU@cluster0.vtwog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("data base connected")
-  client.close();
-});
+
+async function run(){
+  
+}
 
 app.get('/',(req,res)=>{
     console.log("api connected")
